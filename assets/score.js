4ab37a6ae59f64d65c532ad7065579dd9d7b31c1
ArgenX1 <djc.arizona@gmail.com>
2021-09-27 15:35:00 -0700

    ordered highscore list high to low

--- assets/score.js
+++ assets/score.js
@@ -6,13 +6,24 @@ var score
 var scorers = []
 
 function renderScore(){
+    // setting form blank
     scoreList.innerHTML = "";
 
+    scorers.sort(function(a,b){
+        return b.score - a.score
+    })
+    console.log(scorers)
+    // displays array on page
     for( var i = 0; i < scorers.length; i++){
+        // sets scorer to the index of scorers
         var scorer = scorers[i];
         var li = document.createElement("li");
+        // orders list by score highest to lowest
+
+        // takes the key pairs and displays them on the page
         li.textContent = scorer.initials + "  -  " + scorer.score;
         li.setAttribute("data-index", i);
+        console.log(scorer)
         
         scoreList.appendChild(li)
 
@@ -20,11 +31,12 @@ function renderScore(){
 }
 
 function initialize(){
+    // gets "scorers" from local storage
     var storedScorers = JSON.parse(localStorage.getItem("scorers"));
     if(storedScorers !== null){
         scorers = storedScorers;
     }
-
+    // gets count from quiz and stores it to score
     var count = JSON.parse(localStorage.getItem("count"));
     if(count !== null){
         score = count;
